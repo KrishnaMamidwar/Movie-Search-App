d8ff87eec1619aa089dcf3ff0b1b718451dd0d1c
Krishnakant Mamidwar <mamidwarkrishna29@gmail.com>
2023-01-12 14:17:37 +0530

    first commit

---
+++ Movies Search/MovieApp.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function MovieApp() {
+  const [text, setText] = useState("search movie");
+  const [movie, setMovie] = useState([]);
+
+  const changeText = (e) => {
+    // console.log(e)
+    setText(e.target.value);
+  };
+
+  const getMovies = (e) => {
+    e.preventDefault();
+
+    axios
+      .get(`https://www.omdbapi.com/?s=${text}&apikey=94a30c7b`)
+      .then((Response) => {
+        console.log(Response);
+        setMovie(Response.data.Search);
+      });
+  };
+
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Movie App
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Link
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex" onSubmit={getMovies}>
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+                value={text}
+                onChange={changeText}
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+
+      <div className="container">
+        <div className="row">
+          {movie.map((value, i) => {
+            return (
+              <div className="col-3">
+                <div className="card" style={{ width: "18rem" }}>
+                  <img src={value.Poster} className="card-img-top" alt="..." />
+                  <div className="card-body" key={i}>
+                    <h3 className="card-title">{value.Year}</h3>
+                    <h4 className="card-text">
+                      {value.Title}
+                    </h4>
+                    
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MovieApp;
